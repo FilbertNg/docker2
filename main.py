8fae76de41f6bc924d37c9301a107831492a2553
FilbertNg <filbertng1303@gmail.com>
2024-10-06 17:08:40 +0800

    Menyamakan versi

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import joblib
+from service import MLPredictor
+from schema import Request
 
-class Request(BaseModel):
-    petal_length:float
-    petal_width:float
-    sepal_length:float
-    sepal_width:float
+ml_predictor = MLPredictor()
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Fajar M"}
 
-@app.put('/predict')
+@app.post("/predict")
 def predict(req:Request):
-    filename = "model.sav"
-    loaded_model = joblib.load(filename)
-
-    res = loaded_model.predict([[req.sepal_length, req.sepal_width, req.petal_length, req.petal_width]])
-    mapping_label = {0:"Setosa", 1:"Versicolor", 2:"Virginica"}
-    label = mapping_label(res[0])
+    label = ml_predictor.predict(req=req)
 
     return {"prediction": label}
